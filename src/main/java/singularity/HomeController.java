97005d479744e67ede7fa0035c1001bc2e24691a
hyva <sungcheon.lee@outlook.com>
2015-09-29 19:51:02 +0900

    배포 테스트

--- src/main/java/singularity/HomeController.java
+++ src/main/java/singularity/HomeController.java
@@ -1,8 +1,10 @@
 package singularity;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import singularity.user.controller.UserController;
@@ -13,8 +15,8 @@ public class HomeController {
 	UserController userController;
 	
 	@RequestMapping("/")
-	String home() {
-		return "home";
+	String home(Model model, HttpSession session) {
+		return userController.loginForm(model, session);
 	}
 	
 }
