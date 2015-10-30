a3130951b2b2780beeb65209f76a495b6cbf63b7
hwiba <sungcheon.lee@outlook.com>
2015-10-30 18:45:35 +0900

    front에 react/jspm 적용 관련 gitignore 처리

--- src/main/java/me/singularityfor/HomeController.java
+++ src/main/java/me/singularityfor/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Order on 2015. 10. 30..
  */
 @Controller
-public class MainController {
+public class HomeController {
 
     @RequestMapping("/")
     public String home() {
