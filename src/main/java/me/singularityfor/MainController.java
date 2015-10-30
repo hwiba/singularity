c4f6e030aaeafabe22fcf41f530c6a102a0e935c
hwiba <sungcheon.lee@outlook.com>
2015-10-30 16:42:08 +0900

    path error 관련 수정 완료

---
+++ src/main/java/me/singularityfor/MainController.java
@@ -0,0 +1,17 @@
+package me.singularityfor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Order on 2015. 10. 30..
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
