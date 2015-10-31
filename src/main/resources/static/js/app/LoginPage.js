06dab188e98e5f4f8e6d962345420709555458a3
hwiba <sungcheon.lee@outlook.com>
2015-10-31 23:50:39 +0900

    uglifyjs / jspm bundle / ES6 / react / react-dom 적용

---
+++ src/main/resources/static/js/app/LoginPage.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Order on 2015. 10. 23..
+ */
+
+import React from 'react';
+
+
+export default class LoginPage extends React.Component {
+
+    render() {
+        return <h1>LoginPage</h1>
+    }
+}
---
+++ src/main/resources/static/js/app/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Order on 2015. 10. 30..
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import LoginPage from "./LoginPage";
+
+ReactDOM.render(
+    <LoginPage />,
+    document.querySelector('.background')
+)
