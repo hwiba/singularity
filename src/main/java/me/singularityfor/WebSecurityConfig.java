9f83bbcf6c25a67e1707ae9a0f5a5d898e468597
hwiba <sungcheon.lee@outlook.com>
2015-11-17 02:21:42 +0900

    spring boot version update 1.2.7 -> 1.3.0

--- src/main/java/me/singularityfor/WebSecurityConfig.java
+++ src/main/java/me/singularityfor/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @EnableWebMvcSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
